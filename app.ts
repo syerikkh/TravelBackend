d9c8672cbd5944ed3e190796976d1bbbdfd54ebb
syerikkh <147494171+syerikkh@users.noreply.github.com>
2024-10-08 18:53:43 +0800

    deploi

--- app.ts
+++ app.ts
@@ -9,8 +9,22 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://travel-phi-bice.vercel.app",
+];
+
 app.use(
-  cors({ origin: "http://travel-phi-bice.vercel.app", credentials: true })
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
 );
 
 const PORT = 8000;
